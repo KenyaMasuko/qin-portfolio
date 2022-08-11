ae2afd3ff165823c9e1ea840806d676d92d9b515
KenyaMasuko <ken12rug@gmail.com>
2022-08-12 00:42:11 +0900

    HeaderProviderを実装

---
+++ my-app/src/context/Header.tsx
@@ -0,0 +1,20 @@
+import React, { createContext, FC, ReactNode, useState } from "react"
+
+export const HeaderOpenContext = createContext(false)
+export const HeaderOpenToggleContext = createContext(
+  {} as React.Dispatch<React.SetStateAction<boolean>>
+)
+
+export const HeaderOpenProvider: FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [opened, setOpened] = useState<boolean>(false)
+
+  return (
+    <HeaderOpenContext.Provider value={opened}>
+      <HeaderOpenToggleContext.Provider value={setOpened}>
+        {children}
+      </HeaderOpenToggleContext.Provider>
+    </HeaderOpenContext.Provider>
+  )
+}
