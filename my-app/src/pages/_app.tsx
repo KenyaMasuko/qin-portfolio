3021c663fcd8fad5e79237e65bbbfda713217095
KenyaMasuko <ken12rug@gmail.com>
2022-08-12 00:41:42 +0900

    コードの削除と追加

--- my-app/src/pages/_app.tsx
+++ my-app/src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import { MantineProvider } from "@mantine/core"
+import { Footer } from "../components/Footer"
+import { Header } from "../components/Header"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider>
-      <Component {...pageProps} />
+      <Header />
+      <main className="pt-28">
+        <Component {...pageProps} />
+      </main>
+      <Footer />
     </MantineProvider>
   )
 }
