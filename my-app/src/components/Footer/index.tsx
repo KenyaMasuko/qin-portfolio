d725bed0e827abb8aa8f662109c659a9b92dbe10
KenyaMasuko <ken12rug@gmail.com>
2022-08-12 07:48:54 +0900

    ♻️ Footerを修正

--- my-app/src/components/Footer/index.tsx
+++ my-app/src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import React from "react"
 export const Footer = () => {
   return (
     <footer>
-      <div className="pt-6 pb-14 border-solid border-t border-x-0 border-b-0 border-t-gray-400">
+      <div className="pt-6 pb-14 border-solid border-t border-x-0 border-b-0 border-t-gray-200">
         <h2 className="font-bold text-xxs text-gray-400 text-center">
           &copy; 2022 Shimabu IT University
         </h2>
