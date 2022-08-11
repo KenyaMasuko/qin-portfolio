3512463d11686d5c09366d7e2ab6c973a6a928fc
KenyaMasuko <ken12rug@gmail.com>
2022-08-12 07:49:49 +0900

    fontAwesomeに対応

--- my-app/src/pages/_app.tsx
+++ my-app/src/pages/_app.tsx
@@ -4,6 +4,10 @@ import { MantineProvider } from "@mantine/core"
 import { Footer } from "../components/Footer"
 import { Header } from "../components/Header"
 
+import { config } from "@fortawesome/fontawesome-svg-core"
+import "@fortawesome/fontawesome-svg-core/styles.css"
+config.autoAddCss = false
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider>
