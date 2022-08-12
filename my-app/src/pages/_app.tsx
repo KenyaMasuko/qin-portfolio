3d3c924f66f6651525d866e635cc5474dff1be2e
KenyaMasuko <ken12rug@gmail.com>
2022-08-12 21:22:57 +0900

    _appにfontAwesomeの設定を追加

--- my-app/src/pages/_app.tsx
+++ my-app/src/pages/_app.tsx
@@ -8,6 +8,12 @@ import { config } from "@fortawesome/fontawesome-svg-core"
 import "@fortawesome/fontawesome-svg-core/styles.css"
 config.autoAddCss = false
 
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { far } from "@fortawesome/free-regular-svg-icons"
+import { faMoon, faStar } from "@fortawesome/free-solid-svg-icons"
+
+library.add(far, faMoon, faStar)
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider>
