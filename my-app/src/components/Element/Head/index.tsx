f41a0d47587e29cf6439ab723dd0d1886586d344
KenyaMasuko <ken12rug@gmail.com>
2022-08-15 22:02:21 +0900

    meta  imageを設定

--- my-app/src/components/Element/Head/index.tsx
+++ my-app/src/components/Element/Head/index.tsx
@@ -15,7 +15,10 @@ export const MetaHead: FC<MetaHeadProps> = ({
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta property="og:title" content="Shimabu IT University" key="Shimabu" />
-      <meta property="og:image" content="/assets/itkingdom.png" />
+      <meta
+        property="og:image"
+        content="https://qin-portfolio-eta.vercel.app/assets/itkingdom.png"
+      />
       <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
     </Head>
   )
