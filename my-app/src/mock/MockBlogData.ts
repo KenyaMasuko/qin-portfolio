55cf2ef9bcf7a7445ef4181e06ebac5fb4219b7c
KenyaMasuko <ken12rug@gmail.com>
2022-08-15 21:49:17 +0900

    MockBlogDataファイルを追加

---
+++ my-app/src/mock/MockBlogData.ts
@@ -0,0 +1,41 @@
+import { Dispatch, SetStateAction, useState } from "react"
+import { BlogPost } from "src/components/Layout/Top/Blog"
+
+export class MockBlogData {
+  readonly blogPosts: BlogPost[]
+  private readonly setBlogPosts: Dispatch<
+    SetStateAction<{ header: string; description: string; date: string }[]>
+  >
+
+  constructor() {
+    const [blogPosts, setBlogPosts] = useState(this.createDummyPosts())
+    this.blogPosts = blogPosts
+    this.setBlogPosts = setBlogPosts
+  }
+
+  /**
+   * postを新しく生成して、既存のpostに追加する
+   * @return {Boolean} moreDataYet
+   */
+  async loadMorePosts() {
+    await this.delay(2000)
+    const newPosts = this.createDummyPosts()
+    this.setBlogPosts([...this.blogPosts, ...newPosts])
+    return { moreDataYet: this.blogPosts.length < 20 }
+  }
+
+  /**
+   * dummyPostsを生成
+   */
+  private createDummyPosts = () =>
+    [...Array(5)].map((_) => ({
+      header: "This is Header",
+      description:
+        "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.",
+      date: "2022.07.11",
+    }))
+
+  private delay = async (ms: number) => {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+  }
+}
