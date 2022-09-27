7fa5c8e0b666a630e39a03dc020760b1fa0d2f8d
KenyaMasuko <ken12rug@gmail.com>
2022-09-28 07:24:08 +0900

    パスを変更

--- my-app/src/components/Layout/Top/index.tsx
+++ my-app/src/components/Layout/Top/index.tsx
@@ -1,11 +1,11 @@
 import { Box, Container, SimpleGrid } from "@mantine/core";
 import React, { FC } from "react";
 import { Blog } from "src/types/type";
-import { BlogTop } from "./blog";
-import { FirstView } from "./firstView";
-import { GithubTop } from "./github";
-import { PortfolioTop } from "./portfolio";
-import { Twitter } from "./twitter";
+import { BlogTop } from "./Blog";
+import { FirstView } from "./FirstView";
+import { GithubTop } from "./Github";
+import { PortfolioTop } from "./Portfolio";
+import { Twitter } from "./Twitter";
 
 export const Main: FC<{ posts: Blog[] }> = ({ posts }) => {
   return (
