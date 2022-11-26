88b6ee4bd4c8eab2a56ec0d1e68ba8752e2a23ef
kenya12814 <85816730+KenyaMasuko@users.noreply.github.com>
2022-11-26 22:10:22 +0900

    環境変数の型を定義

---
+++ my-app/global.d.ts
@@ -0,0 +1,12 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly MICROCMS_API_KEY;
+    readonly MICROCMS_SERVICE_DOMAIN;
+    readonly TWITTER_API_KEY;
+    readonly TWITTER_API_KEY_SECRET;
+    readonly TWITTER_BEARER_TOKEN;
+    readonly TWITTER_USER_ID;
+    readonly GITHUB_TOKEN;
+    readonly GITHUB_USER_NAME;
+  }
+}
