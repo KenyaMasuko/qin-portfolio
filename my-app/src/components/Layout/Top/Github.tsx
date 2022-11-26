2826c21d371942432d53db2b009f1c51d272bffd
kenya12814 <85816730+KenyaMasuko@users.noreply.github.com>
2022-11-26 22:10:35 +0900

    GraphQLでAPI叩いた

--- my-app/src/components/Layout/Top/Github.tsx
+++ my-app/src/components/Layout/Top/Github.tsx
@@ -1,47 +1,88 @@
 import { faCodeFork } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Center, Progress, Title } from "@mantine/core";
-import React from "react";
 import { LinkButton } from "src/components/Element/LinkButton";
 import { HeadingTitle } from "src/components/Element/Title";
+import { RepositoryResponse } from "src/pages/api/github";
+import { useGithubRepo } from "src/utils/githubClient";
 
 export const GithubTop = () => {
+  const { data, error } = useGithubRepo();
+  if (error) return <div>{error.message}</div>;
+  if (!data) return <div>loading...</div>;
+
+  console.log(
+    data.map((repo) =>
+      repo.languages.edges
+        .sort((a, b) => b.size - a.size)
+        .map((lang) => ({
+          value: Number(
+            ((lang.size / repo.languages.totalSize) * 100).toFixed(1)
+          ),
+          color: lang.node.color,
+        }))
+    )
+  );
+
   return (
     <section>
       <HeadingTitle>Github</HeadingTitle>
       <div className="space-y-6">
-        {[...Array(5)].map((_, index) => (
-          <article key={index} className="space-y-4">
+        {data.map((repo: RepositoryResponse) => (
+          <article key={repo.name} className="space-y-4">
             <Title order={2} className="text-lg my-0">
-              lightsound/nexst-tailwind
+              {repo.name}
             </Title>
-            <p className="text-base my-0">Next.js starter template.</p>
+            {repo.description && (
+              <p className="text-base my-0">{repo.description}</p>
+            )}
+            <p className="text-base my-0"></p>
             <div className="text-xs text-gray-400 font-bold space-x-2 flex">
               <div>
                 <FontAwesomeIcon icon={["far", "star"]} />
-                <span>117</span>
+                <span>{repo.stars}</span>
               </div>
               <div>
                 <FontAwesomeIcon icon={faCodeFork} />
-                <span>18</span>
+                <span>{repo.forks}</span>
               </div>
             </div>
             <Progress
               size="md"
               radius="xl"
-              sections={[
-                { value: 65, color: "#3178C6" },
-                { value: 30, color: "#F1E05A" },
-                { value: 5, color: "#EDEDED" },
-              ]}
+              sections={repo.languages.edges
+                .sort((a, b) => b.size - a.size)
+                .map((lang) => ({
+                  value: Number(
+                    ((lang.size / repo.languages.totalSize) * 100).toFixed(1)
+                  ),
+                  color: lang.node.color,
+                }))}
             />
             <div className="flex space-x-3">
-              <span className="flex items-center text-xs font-bold">
-                <span className="bg-[#3178C6] w-2 h-2 absolute rounded-full"></span>
-                <span className="ml-3">TypeScript</span>
-                <span className="text-gray-400 ml-2">65%</span>
-              </span>
-              <span className="flex items-center text-xs font-bold">
+              {repo.languages.edges
+                .sort((a, b) => b.size - a.size)
+                .map((lang) => {
+                  const ratio = (
+                    (lang.size / repo.languages.totalSize) *
+                    100
+                  ).toFixed(1);
+                  return (
+                    <>
+                      <span className="flex items-center text-xs font-bold">
+                        {/* <span className="bg-[#3178C6] w-2 h-2 absolute rounded-full"></span> */}
+                        <span
+                          className={`bg-[${lang.node.color}] w-2 h-2 absolute rounded-full`}
+                        ></span>
+
+                        <span className="ml-3">{lang.node.name}</span>
+                        <span className="text-gray-400 ml-2">{ratio}%</span>
+                      </span>
+                    </>
+                  );
+                })}
+
+              {/* <span className="flex items-center text-xs font-bold">
                 <span className="bg-[#F1E05A] w-2 h-2 absolute rounded-full"></span>
                 <span className="ml-3">JavaScript</span>
                 <span className="text-gray-400 ml-2">30%</span>
@@ -50,7 +91,7 @@ export const GithubTop = () => {
                 <span className="bg-[#EDEDED] w-2 h-2 absolute rounded-full"></span>
                 <span className="ml-3">Other</span>
                 <span className="text-gray-400 ml-2">5%</span>
-              </span>
+              </span> */}
             </div>
           </article>
         ))}
---
+++ my-app/src/pages/api/github.ts
@@ -0,0 +1,76 @@
+import { gql, GraphQLClient } from "graphql-request";
+import { NextApiRequest, NextApiResponse } from "next";
+
+type ApiResponse = {
+  repositoryOwner: {
+    repositories: {
+      nodes: RepositoryResponse[];
+    };
+  };
+};
+
+export type RepositoryResponse = {
+  name: string;
+  description: string | null;
+  forks: number;
+  stars: number;
+  languages: {
+    totalSize: number;
+    edges: {
+      size: number;
+      node: {
+        name: string;
+        color: string;
+      };
+    }[];
+  };
+};
+
+const getGithubRepo = async (
+  req: NextApiRequest,
+  res: NextApiResponse<RepositoryResponse[]>
+) => {
+  const query = gql`
+    query {
+      repositoryOwner(login: "KenyaMasuko") {
+        repositories(last: 3) {
+          nodes {
+            name
+            description
+            forks: forkCount
+            stars: stargazerCount
+            languages(first: 10) {
+              totalSize
+              edges {
+                size
+                node {
+                  name
+                  color
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const client = new GraphQLClient("https://api.github.com/graphql", {
+    headers: {
+      authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+    },
+  });
+  const rawRepoInfo = await client.request<ApiResponse>(query);
+  const repoInfo = rawRepoInfo.repositoryOwner.repositories.nodes.map(
+    (repo) => ({
+      name: repo.name,
+      description: repo.description,
+      forks: repo.forks,
+      stars: repo.stars,
+      languages: repo.languages,
+    })
+  );
+  res.status(200).send(repoInfo);
+};
+
+export default getGithubRepo;
